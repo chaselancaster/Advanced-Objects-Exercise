71701e4758cee5911e63d72e7e4caeeef1f7825e
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-12 15:40:59 -0700

    add #3 answer

--- exercise6.js
+++ exercise6.js
@@ -19,3 +19,22 @@ object1.a = 4;
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+    constructor(name, type, color) {
+        this.name = name
+        this.type = type
+        this.color = color
+    }
+}
+
+class Mamal extends Animal {
+    constructor(name, type, color) {
+        super(name, type, color)
+    }
+    speak() {
+        console.log(`Moooo I am a ${type}. My name is ${name} and my color is ${color}.`)
+    }
+}
+
+const cow = new Mamal('cow', 'chase', 'black')
